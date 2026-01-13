16a8ee74cbd4b39aa0c340eec5b79cc97a3a12a1
AOKA666 <60079319+AOKA666@users.noreply.github.com>
2026-01-13 11:28:28 +0800

    更新站点地图

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.willschoolbeclosed.com' // 请替换为你的实际域名
+  const baseUrl = 'https://www.willschoolbeclosed.online' // 请替换为你的实际域名
 
   return [
     {
